b0d364ccb1392831ec603e06a6b872fa768ad815
Fiuza <fiuzagr@gmail.com>
2019-03-24 23:46:27 +0000

    Install script && XFCE configs

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+
+{
+
+set -eu
+
+umask 022
+
+oka_clone() {
+  cd $HOME
+  git init
+  git remote add origin https://github.com/iaguara/oka.git
+  git pull origin master
+  cd -
+}
+
+oka_configure_git() {
+  printf "\
+[include]\n\
+  path = ${HOME}/.oka/git/.gitconfig\n\
+" | tee ${HOME}/.gitconfig
+}
+
+oka_install_omz() {
+  git clone \
+    --quiet \
+    --depth=1 \
+    --single-branch \
+    -b master \
+    https://github.com/robbyrussell/oh-my-zsh.git \
+    ${HOME}/.oh-my-zsh
+}
+
+oka_install_tpm() {
+  git clone \
+    --quiet \
+    --depth=1 \
+    --single-branch \
+    -b master \
+    https://github.com/tmux-plugins/tpm.git \
+    ${HOME}/.tmux/plugins/tpm
+}
+
+oka_install() {
+  oka_clone
+  oka_configure_git
+  oka_install_omz
+  oka_install_tpm
+}
+oka_install
+
+}
